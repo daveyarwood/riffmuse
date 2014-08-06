0e22d56e1b7aff2627ab29992a88b876a1e02df7
Dave Yarwood <dave.yarwood@gmail.com>
2014-08-05 23:02:08 -0400

    privatize note to number map

--- src/riffmuse/ascii.clj
+++ src/riffmuse/ascii.clj
@@ -16,7 +16,7 @@
                                                   \space)))]
     (str/join \newline [beat-line result])))
 
-(def note->number
+(def ^:private note->number
   {"Cb" 11, "C" 0, "C#" 1, "Db" 1, "D" 2, "D#" 3, "Eb" 3, "E" 4, "E#" 5,
    "Fb" 4, "F" 5, "F#" 6, "Gb" 6, "G" 7, "G#" 8, "Ab" 8, "A" 9, "A#" 10,
    "Bb" 10, "B" 11, "B#" 0})
