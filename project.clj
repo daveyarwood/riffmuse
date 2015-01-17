446279735ae8962dfdcdf1bf1ceefe3f24087007
Dave Yarwood <dave.yarwood@gmail.com>
2015-01-17 11:09:52 -0500

    bump deps

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://www.github.com/daveyarwood/riffmuse"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [instaparse "1.3.2"]
-                 [trptcolin/versioneer "0.1.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [instaparse "1.3.5"]
+                 [trptcolin/versioneer "0.1.1"]]
   :aot [riffmuse.core]
   :main riffmuse.core)
